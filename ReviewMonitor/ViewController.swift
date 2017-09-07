535773e0a9ed445d76d457d6acfe36db67e704ef
Rishabh Tayal <rtayal11@gmail.com>
2017-09-07 16:32:02 -0500

    initial commit

---
+++ ReviewMonitor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReviewMonitor
+//
+//  Created by Tayal, Rishabh on 9/7/17.
+//  Copyright © 2017 Tayal, Rishabh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
