8ca9c9ba80e02f60adf03587cd5c4e25c0e1ce0c
Rishabh Tayal <rtayal11@gmail.com>
2017-09-08 16:36:34 -0500

    Added login view controller

---
+++ ReviewMonitor/Login ViewController/LoginViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LoginViewController.swift
+//  ReviewMonitor
+//
+//  Created by Tayal, Rishabh on 9/8/17.
+//  Copyright © 2017 Tayal, Rishabh. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func signinTapped(_ sender: Any) {
+    }
+}
