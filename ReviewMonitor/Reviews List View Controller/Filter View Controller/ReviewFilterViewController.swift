71900901491b609b3c244e6b23a0922b5b633f32
Rishabh Tayal <rtayal11@gmail.com>
2017-09-17 23:25:06 -0500

    commti

---
+++ ReviewMonitor/Reviews List View Controller/Filter View Controller/ReviewFilterViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ReviewFilterViewController.swift
+//  ReviewMonitor
+//
+//  Created by Tayal, Rishabh on 9/17/17.
+//  Copyright © 2017 Tayal, Rishabh. All rights reserved.
+//
+
+import UIKit
+
+class ReviewFilterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
