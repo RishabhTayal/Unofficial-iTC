e0c03aadd04a5d89d174e1c8daa21f3124a54e2e
Rishabh Tayal <rtayal11@gmail.com>
2018-01-03 16:24:13 -0600

    Added date format extension

--- ReviewMonitor/AppHelper.swift
+++ ReviewMonitor/AppHelper.swift
@@ -8,15 +8,27 @@
 
 import UIKit
 
+enum DateFormat: String {
+    case MMMddyyy = "MMM dd, yyyy"
+}
+
 extension UIView {
     public func addBorder(_ width: CGFloat, color: UIColor) {
         layer.borderColor = color.cgColor
         layer.borderWidth = width
     }
-    
+
     public func cornerRadius(_ radius: CGFloat) {
         layoutIfNeeded()
         layer.cornerRadius = radius
         clipsToBounds = true
+    }
+}
+
+extension Date {
+    func formatDate(format: DateFormat) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = format.rawValue
+        return formatter.string(from: self)
     }
 }
